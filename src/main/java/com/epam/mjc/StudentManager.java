666f6c4d4ba17dcc2a43b0d227a12c4ca0aef567
AlokhinaTetiana <aliokhinatania03@gmail.com>
2023-07-06 23:11:46 +0300

    k

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,7 +6,9 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find (long studentID) throws StudentNotFoundException{
-    try {return Student.getValueOf(studentID);}
+    try {
+      return Student.getValueOf(studentID);
+    }
     catch (IllegalArgumentException ex){
       throw new StudentNotFoundException("Could not find student with ID "+studentID,ex);
     }
@@ -16,8 +18,14 @@ public class StudentManager {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try {
+
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
+      catch (StudentNotFoundException ex) {
+      System.out.print(ex);
+      }
     }
 
   }
